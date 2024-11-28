ae80e6881d0dbffe2b2713e24c0279a89a589052
PirateFlix <Lucasthedevv@gmail.com>
2024-11-28 12:00:00 +0500

    Updated config files

--- config/app.php
+++ config/app.php
@@ -139,7 +139,7 @@ return [
     'providers' => [
 
         /*
-         * Laravel Framework Service Providers
+         * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
